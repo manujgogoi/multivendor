c25d1c43e70e386617a7dc96998a87c64a61e814
Manuj Gogoi <manujgogoi@gmail.com>
2021-09-25 07:59:17 +0530

    Initial django project setup
    > Use Pipenv (pip+virtualenv)
    > Django project created: 'core'
    > 'account' app is created
    > Custom user model is created
    > Configure MySQL database
    > Configure AUTH_USER_MODEL & TIME_ZONE settings
    > No migrations created.

---
+++ account/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+)
+from django.utils.translation import ugettext_lazy as _
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin="False"):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+
+        user_obj = self.model(
+            email = self.normalize_email(email)
+        )
+        user_obj.active = is_active
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.set_password(password)
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(email, password=password, is_staff=True)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password=password, is_staff=True, is_admin=True)
+        return user
+
+class User(AbstractBaseUser):
+    email       = models.EmailField(_('email id'), max_length=255, unique=True)
+    active      = models.BooleanField(_('is active'), default=True) # Can login
+    staff       = models.BooleanField(_('staff status'), default=False) # Staff user non Super
+    admin       = models.BooleanField(_('is admin'), default=False) # Superuser
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # USERNAME_FIELD and password are required by default
+
+
+    objects = UserManager()
+
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
+
+    
