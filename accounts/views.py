dc1fd715d0fc4132b1bbac0ff83d12c52f3b2bdd
Manuj Gogoi <manujgogoi@gmail.com>
2021-10-21 06:47:37 +0530

    Added django-allauth feature

--- accounts/views.py
+++ accounts/views.py
@@ -6,38 +6,38 @@ from .forms import LoginForm, RegisterForm
 User = get_user_model()
 
 # Create your views here.
-def register_view(request):
-    form = RegisterForm(request.POST or None)
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        password1 = form.cleaned_data.get("password1")
-        password2 = form.cleaned_data.get("password2")
-        try:
-            user = User.objects.create_user(email, password1)
-        except:
-            user = None,
-        if user != None:
-            login(request, user)
-            return redirect("/")
-        else:
-            request.session['register_error'] = 1 # 1 == True
-    return render(request, "accounts/register.html", {"form": form})
+# def register_view(request):
+#     form = RegisterForm(request.POST or None)
+#     if form.is_valid():
+#         email = form.cleaned_data.get("email")
+#         password1 = form.cleaned_data.get("password1")
+#         password2 = form.cleaned_data.get("password2")
+#         try:
+#             user = User.objects.create_user(email, password1)
+#         except:
+#             user = None,
+#         if user != None:
+#             login(request, user)
+#             return redirect("/")
+#         else:
+#             request.session['register_error'] = 1 # 1 == True
+#     return render(request, "accounts/register.html", {"form": form})
 
 
-def login_view(request):
-    form = LoginForm(request.POST or None)
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, email=email, password=password)
+# def login_view(request):
+#     form = LoginForm(request.POST or None)
+#     if form.is_valid():
+#         email = form.cleaned_data.get("email")
+#         password = form.cleaned_data.get("password")
+#         user = authenticate(request, email=email, password=password)
 
-        if user != None:
-            login(request, user)
-            return redirect('/')
-        else:
-            request.session['invalid_user'] = 1 # 1 = True
-    return render(request, "accounts/login.html", {"form": form})
+#         if user != None:
+#             login(request, user)
+#             return redirect('/')
+#         else:
+#             request.session['invalid_user'] = 1 # 1 = True
+#     return render(request, "accounts/login.html", {"form": form})
 
-def logout_view(request):
-    logout(request)
-    return redirect("/login")
+# def logout_view(request):
+#     logout(request)
+#     return redirect("/login")
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('vendor/',include("vendor.urls")),
     path('admin/', admin.site.urls),
 ]
